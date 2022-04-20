a4bb12a47d092c0606a53a87442aeade10907592
RamonAPereira <ramonpereiranzd@gmail.com>
2022-04-20 01:10:21 -0300

    build: configuração base do darkmode

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,5 +1,22 @@
+<script setup></script>
+
 <template>
   <section class="header">
-    <h1>Where in the world?</h1>
+    <h1 class="header__title">Where in the world?</h1>
+    <button class="toggle" @click="$emit('toggle')">Dark Mode</button>
   </section>
 </template>
+
+<style scoped>
+.header {
+  @apply flex justify-between px-5 py-6  shadow-lg dark:bg-slate-800;
+}
+
+.header__title {
+  @apply font-semibold dark:text-white;
+}
+
+.toggle {
+  @apply dark:text-white;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,7 +1,15 @@
 <script setup>
 import TheHeader from "../components/layout/TheHeader.vue";
+import { ref } from "vue";
+
+const isDark = ref(false);
+const toggleDarkMode = function () {
+  isDark.value = !isDark.value;
+};
 </script>
 
 <template>
-  <the-header></the-header>
+  <div :class="{ dark: isDark }">
+    <the-header @toggle="toggleDarkMode"></the-header>
+  </div>
 </template>
