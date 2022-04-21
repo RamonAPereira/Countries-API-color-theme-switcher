cf11a559d0f69600faf48d35ccd88789e32256ef
RamonAPereira <ramonpereiranzd@gmail.com>
2022-04-21 15:32:33 -0300

    build: adicionei rota para o CountryDetails

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/HomePage.vue";
+import CountryDetails from "./pages/CountryDetails.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/home" },
     { path: "/home", component: HomePage },
+    { path: "/country-details", component: CountryDetails },
   ],
 });
 
