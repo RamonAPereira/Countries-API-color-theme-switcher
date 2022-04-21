237ed33a58d2323a72af28e4c3407589e69c0007
RamonAPereira <ramonpereiranzd@gmail.com>
2022-04-21 20:58:56 -0300

    build: rota inamica para os detalhes de cada país

--- src/router.js
+++ src/router.js
@@ -8,7 +8,8 @@ const router = createRouter({
   routes: [
     { path: "/", redirect: "/home" },
     { path: "/home", component: HomePage },
-    { path: "/country-details", component: CountryDetails },
+    { path: "/country-details/:flag", component: CountryDetails },
+    { path: "/catchAll(.*)", component: HomePage },
   ],
 });
 
